77f769f1796db205f415fa3b636f7dab2ad20061
Mamoru Hashimoto <mamoru.hashimoto@hotmail.com>
2016-06-13 12:28:40 +0900

    first commit.

---
+++ public/js/script.js
@@ -0,0 +1,99 @@
+var myApp = angular.module('simpleApp',[]);
+// Angular sample code#3
+myApp.controller('simpleController', ['$scope', function($scope) {
+  // Initialize the model variables
+  $scope.firstName = "John";
+  $scope.lastName = "Doe";
+
+  // Define utility functions
+  $scope.getFullName = function () {
+    return $scope.firstName + " " + $scope.lastName;
+  };
+}]);
+
+// Angular sample code#4
+myApp.controller('firstController', [ '$scope', function($scope) {
+  // Initialize the model variables
+  $scope.firstName = "John";
+  $scope.lastName = "Doe";
+ 
+  // Define utility functions
+  $scope.getFullName = function() {
+    return $scope.firstName + " " + $scope.lastName;
+  };
+}]);
+
+myApp.controller('secondController', [ '$scope', function($scope) {
+  // Initialize the model variables
+  $scope.firstName = "Bob";
+  $scope.middleName = "Al";
+  $scope.lastName = "Smith";
+
+  // Define utility functions
+  $scope.getFullName = function () {
+    return $scope.firstName + " " + $scope.middleName + " " + $scope.lastName;
+  };
+}]);
+
+// Angular sample code#5
+myApp.controller('firstControllerScope', [ '$scope', function($scope) {
+  // Initialize the model variables
+  $scope.firstName = "John";
+}]);
+
+myApp.controller('secondControllerScope', [ '$scope', function($scope) {
+  // Initialize the model variables
+  $scope.lastName = "Doe";
+
+  // Define utility functions
+  $scope.getFullName = function() {
+    return $scope.firstName + " " + $scope.lastName;
+  };
+}]);
+
+myApp.controller('thirdControllerScope', [ '$scope', function($scope) {
+  // Initialize the model variables
+  $scope.middleName = "Al";
+  $scope.lastName = "Smith";
+
+  // Define utility functions
+  $scope.getFullName = function() {
+    return $scope.firstName + " " + $scope.middleName + " " + $scope.lastName;
+  };
+}]);
+
+myApp.controller('firstControllerObj', [ '$scope', function($scope) {
+  // Initialize the model object
+  $scope.firstModelObj = {
+    firstName: "John"
+  };
+}]);
+
+myApp.controller('secondControllerObj', [ '$scope', function($scope) {
+  // Initialize the model object
+  $scope.secondModelObj = {
+    lastName: "Doe"
+  };
+
+  // Define utility functions
+  $scope.getFullName = function() {
+    return $scope.firstModelObj.firstName + " " +
+      $scope.secondModelObj.lastName;
+  };
+}]);
+
+myApp.controller('thirdControllerObj', [ '$scope', function($scope) {
+  // Initialize the model object
+  $scope.thirdModelObj = {
+    middleName: "Al",
+    lastName: "Smith"
+  };
+
+  // Define utility functions
+  $scope.getFullName = function() {
+    return $scope.firstModelObj.firstName + " " +
+      $scope.thirdModelObj.middleName + " " +
+      $scope.thirdModelObj.lastName;
+  };
+}]);
+
---
+++ public/js/script2.js
@@ -0,0 +1,78 @@
+// Angular sample code#6
+// A simple module with no dependencies
+angular.module("mainModule", [])
+.controller("simpleController", function ($scope)
+{
+  // Initialize the model
+  $scope.person = {
+    firstName: "John",
+    lastName: "Doe",
+
+    // Define utility functions on the model object
+    getFullName: function ()
+    {
+      return this.firstName + " " + this.lastName;
+    }
+  };
+});
+
+// Angular sample code#7
+// Register an object instance as a value and name it "person"
+angular.module("mainModule2", [])
+.value("person", 
+{
+  firstName: "",
+  lastName: "",
+
+  getFullName: function ()
+  {
+    return this.firstName + " " + this.lastName;
+  }
+})
+// Get the "person" registered object instance through Dependency Injection
+.controller("mainController", function ($scope, person)
+{
+  person.firstName = "John";
+  person.lastName = "Doe";
+
+  // Set a variable on the scope to reference the "person" instance
+  // from the HTML template.
+  $scope.personInstance = person;
+});
+
+// Angular sample code#8
+// Definition of a PersonManager object
+var PersonManager = function (person)
+{
+  this.personInstance = person;
+};
+
+PersonManager.prototype.getPersonFirstName = function ()
+{
+  return this.personInstance.firstName;
+};
+
+PersonManager.prototype.getPersonLastName = function ()
+{
+  return this.personInstance.lastName;
+};
+
+PersonManager.prototype.getPersonFullName = function (separator)
+{
+  return this.personInstance.firstName + separator + this.personInstance.lastName;
+};
+
+// Initialization of the "mainModule"
+angular.module("mainModule3", [])
+  .value("person", {
+  firstName: "",
+  lastName: ""
+})
+  .service("personManager", PersonManager) 
+  .controller("mainController", function ($scope, person, personManager)
+{
+  person.firstName = "John";
+  person.lastName = "Doe";
+  $scope.personManagerInstance = personManager;
+});
+
